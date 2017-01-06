30fc548d2ad6533e7797e2a3412f3e5d987b9504
80883080@qq.com <80883080@qq.com>
2017-01-06 13:28:39 +0800

    地图类三个页面提交

--- svn/js/pageController.js
+++ svn/js/pageController.js
@@ -1,4 +1,5 @@
-var pageName = ["index","aboutUs","askPage","classRoom","consult","doctor","doctorInfo","navigation","vip"];
+var pageName = ["index","aboutUs","askPage","classRoom","consult","doctor","doctorInfo","navigation","vip","classInfo","videoInfo","location"];
+
 if(mui(".foot div")[0]){
 	mui(".foot div")[0].addEventListener("tap",function(){
 		var webview = mui.openWindow({
@@ -94,17 +95,73 @@ if(mui(".askBtn")[0]){
 
 if(mui(".doctor")){
 	for(var i=0;i<mui(".doctor").length;i++){
-		addEvent(i);
+		addEventD(i);
 	}
 }
 
-function addEvent(num){
+function addEventD(num){
 	mui(".doctor")[num].addEventListener("tap",function(){
 		window.localStorage.doctorId = mui(".doctor")[num].id;
 		console.log(localStorage["doctorId"])
 		var webview = mui.openWindow({
 			url:pageName[6]+".html",
 			id :pageName[6],
+			extras:{
+			}
+		});
+	})
+}
+
+if(mui(".room")){
+	for(var i=0;i<mui(".room").length;i++){
+		addEventR(i);
+	}
+}
+
+function addEventR(num){
+	mui(".room")[num].addEventListener("tap",function(){
+		window.localStorage.doctorId = mui(".room")[num].id;
+		console.log(localStorage["room"])
+		var webview = mui.openWindow({
+			url:pageName[9]+".html",
+			id :pageName[9],
+			extras:{
+			}
+		});
+	})
+}
+if(mui(".video")){
+	for(var i=0;i<mui(".video").length;i++){
+		addEventV(i);
+	}
+}
+
+function addEventV(num){
+	mui(".video")[num].addEventListener("tap",function(){
+		window.localStorage.doctorId = mui(".video")[num].id;
+		console.log(localStorage["video"])
+		var webview = mui.openWindow({
+			url:pageName[10]+".html",
+			id :pageName[10],
+			extras:{
+			}
+		});
+	})
+}
+
+if(mui(".hos")){
+	for(var i=0;i<mui(".hos").length;i++){
+		addEventH(i);
+	}
+}
+
+function addEventH(num){
+	mui(".hos")[num].addEventListener("tap",function(){
+		window.localStorage.doctorId = mui(".hos")[num].id;
+		console.log(localStorage["hos"])
+		var webview = mui.openWindow({
+			url:pageName[11]+".html",
+			id :pageName[11],
 			extras:{
 			}
 		});
@@ -151,3 +208,5 @@ getJsonAccess.cacheImg = function(dom){
 		imgArr[i].src="img/noneImg.png"
 	}
 }
+
+
