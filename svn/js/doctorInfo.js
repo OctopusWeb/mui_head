3a5aa157619f1f9fb9d2bb75496555a437c49df8
80883080@qq.com <80883080@qq.com>
2017-01-11 10:33:51 +0800

    svn版本整合

--- svn/js/doctorInfo.js
+++ svn/js/doctorInfo.js
@@ -5,22 +5,33 @@ window.onload = function(){
 	function statusFun(bol){
 		if(bol){
 			getJsonAccess.cacheData(localStorage["doctorInfo"],drawHtml,jsonDom);
-			setTimeout(function(){statusFun(false)},10);
+			setTimeout(function(){statusFun(false)},1000);
 		}else{
 			var data = '?apk='+localStorage["apk"]+'&id='+localStorage["doctorId"]
-			getJsonAccess.getJson('/s/dr_info',data,drawHtml);
+			getJsonAccess.getJson('nzf2/s/dr_info',data,drawHtml);
 		}
 	}
 
 	function drawHtml(data){
 		if(typeof(data) == "string"){data = JSON.parse(data)}
 		var json = data.datas;
+		localStorage.doctorPic = json.photo;
+		localStorage.doctorName = json.name;
+		localStorage.doctorHonor = json.honor;
 		var index='<div class="doctorHead"><img src="'+json.photo+'"/><h1>'+json.name+
 					'</h1><h2>'+json.honor+'</h2> </div><div class="doctorInfo">'+
 					'<i class="icon-shoucang iconfont"></i><h1>医生简介</h1><p>'+
-					json.intro+'</p></div><div class="bottomBtn"><h1>'+
+					json.intro+'</p></div><div class="bottomBtn"><h1 class="yuyue">'+
 					'<span class="mui-icon mui-icon-chat"></span>我要预约</h1></div>'
 		jsonDom.innerHTML = index;
+		mui(".yuyue")[0].addEventListener("tap",function(){
+			var webview = mui.openWindow({
+				url:pageName[12]+".html",
+				id :pageName[12],
+				extras:{
+				}
+			});
+		})
 		window.localStorage.doctorInfo = JSON.stringify(data);
 	}
 }
